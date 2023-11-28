d30b319b6ead556e6ffa1ae341867ae3ddc79fd9
Luky-probably-not <jb.bodusseau@gmail.com>
2023-11-28 08:27:42 +0100

    change test

--- txt.go
+++ txt.go
@@ -6,11 +6,12 @@ import (
 	"os"
 )
 
-func SelectWord(s string) []string { // function that selects a random word from the library
+
+func SelectWord(s string) []string {
 	content, err := os.ReadFile(s)
 	if err != nil {
 		fmt.Println("file error")
-		return []string(nil)
+		return []string{}
 	}
 	list := byteToString(content)
 	mot := list[rand.Intn(len(list)-1)]
@@ -22,7 +23,7 @@ func SelectWord(s string) []string { // function that selects a random word from
 	return str
 }
 
-func byteToString(b []byte) []string {//function that converts the content of a game library into a string array
+func byteToString(b []byte) []string {
 	word := ""
 	result := []string{}
 	for _, i := range b {
@@ -33,5 +34,6 @@ func byteToString(b []byte) []string {//function that converts the content of a 
 			word += string(i)
 		}
 	}
+	result = append(result, word)
 	return result
 }
